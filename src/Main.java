510bd311db9218f4ba98626f25343a69d0ad101c
Elizaveta <aliza-97@mail.ru>
2023-10-31 22:59:51 +0300

    correct mistake

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         int first = 100;
-        int cash = 1000;
-        int x = 100;
+        int cash = 700;
+        int bonus = 100;
 
-        if (cash+first > 1000) {
-            System.out.println("Сумма на счету " + (first+cash) + " рублей");
-            System.out.println("Сумма вместе с бонусами составляет " + ((cash/x)+cash+first));
+        if (cash + first > 1000) {
+            System.out.println("Сумма на счету " + (first + cash) + " рублей");
+            System.out.println("Сумма вместе с бонусами составляет " + ((cash / bonus) + cash + first));
         } else {
-            System.out.println("Сумма на счету " + (first+cash) + " рублей");
+            System.out.println("Сумма на счету " + (first + cash) + " рублей");
         }
 
 
